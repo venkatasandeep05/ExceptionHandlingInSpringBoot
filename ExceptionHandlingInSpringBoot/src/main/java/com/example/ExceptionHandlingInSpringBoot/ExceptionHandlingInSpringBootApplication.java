6766ae707776db9aef463ca1c60b7263c7b9624d
venkatasandeep05 <venkatasandeep05@gmail.com>
2025-07-27 22:16:08 +0530

    added template project

---
+++ ExceptionHandlingInSpringBoot/src/main/java/com/example/ExceptionHandlingInSpringBoot/ExceptionHandlingInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExceptionHandlingInSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingInSpringBootApplication.class, args);
+	}
+
+}
---
+++ ExceptionHandlingInSpringBoot/src/test/java/com/example/ExceptionHandlingInSpringBoot/ExceptionHandlingInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExceptionHandlingInSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
